5054b6c5ae43d52760a476a689d8a94980b5dee6
张德志 <zhangdezhi@zhangdezhideMacBook-Air.local>
2018-08-30 21:14:17 +0800

    引入react react-redux

--- src/index.js
+++ src/index.js
@@ -1,7 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import logger from 'redux-logger';
 import App from './components/App';
+import { Provider } from 'react-redux';
+import { createStore,applyMiddleware } from 'redux';
+import rootReducers from './reducers/index'
 import registerServiceWorker from './registerServiceWorker';
+import { composeWithDevTools } from 'redux-devtools-extension';
+const store = createStore(rootReducers,composeWithDevTools(
+    applyMiddleware(logger)
+));
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+ document.getElementById('root'));
 registerServiceWorker();
