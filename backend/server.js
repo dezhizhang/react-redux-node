62f2534426d623d6b91784e44401a932cda56019
张德志 <zhangdezhi@zhangdezhideMacBook-Air.local>
2018-08-30 23:50:04 +0800

    引入数据库mongodb

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,30 @@
 import express from 'express';
+import mongodb from 'mongodb';
 
 let app = express();
+let dbUrl='mongodb://localhost:27017/crud';
+
+mongodb.MongoClient.connect(dbUrl,(err,client)=>{
+    if(err){
+        console.log('数据库连接失败');
+        return;
+
+    }
+    console.log('数据库连接成功');
+
+    let db=client.db('crud');
+    app.get('/api/games',(req,res)=>{
+        db.collection('games').find({}).toArray((err,games)=>{
+            if(err){
+                console.log(err);
+                return;
+
+            }
+
+            res.json(games);
+
+        })
+    })
+})
 
 app.listen('8082','localhost',()=>console.log('Server is run localhost:8082'));
